cd694cc5d4a8620ed6deaf4d4b9151d3d702b12f
Hasanuzzaman <hasanuzzamanbe@gmail.com>
2020-06-29 20:16:48 +0600

    scss added, code beautified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
-import "./App.css";
+import "./App.scss";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 3,
+      counter: 3,
     };
-   this.counterChange = this.counterChange.bind(this);
+    this.counterChange = this.counterChange.bind(this);
   }
 
   counterChange(event) {
@@ -25,10 +25,10 @@ class App extends Component {
 
   }
 
- 
+
 
   render() {
-    const {counter} = this.state;
+    const { counter } = this.state;
     const multiplicationTables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const tableStyle = {
       border: "2px solid black",
@@ -40,31 +40,31 @@ class App extends Component {
 
     return (
       <div className="App">
-            <h1> Multiplication App </h1>
-            <input
-              name="counter"
-              onChange={this.counterChange}
-              value={counter}
-              type="number"
-              style={{width: "14%", height: "30px", fontSize: "25px"}}
-            />
-            <br /> <br />
-          
-            <table style={tableStyle}>
-                <tbody>
-                    <tr>
-                      <th>Multiplication Table</th>
-                    </tr>
-                    {multiplicationTables.map((multiplicationTable, index) => (
-                      <tr key={index}>
-                        <td style={tableStyle}>
-                          {multiplicationTable} * {counter ? counter : 0} = {multiplicationTable * counter}
-                        </td>
-                      </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+        <h1> Multiplication App </h1>
+        <input
+          name="counter"
+          onChange={this.counterChange}
+          value={counter}
+          type="number"
+          style={{ width: "14%", height: "30px", fontSize: "25px" }}
+        />
+        <br /> <br />
+
+        <table style={tableStyle}>
+          <tbody>
+            <tr>
+              <th>Multiplication Table</th>
+            </tr>
+            {multiplicationTables.map((multiplicationTable, index) => (
+              <tr key={index}>
+                <td style={tableStyle}>
+                  {multiplicationTable} * {counter ? counter : 0} = {multiplicationTable * counter}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
