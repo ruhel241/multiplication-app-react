dfeb485059cd62e12d0a2e3e7ce864e22d834e5b
Ruhel <ruhel241@gmail.com>
2020-08-07 18:46:45 +0600

    UI implements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
 import React, { Component } from "react";
 import "./App.scss";
 
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import TextField from '@material-ui/core/TextField';
+
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
 class App extends Component {
   constructor(props) {
     super(props);
@@ -18,8 +32,6 @@ class App extends Component {
     });
   }
 
-
-
   render() {
     const { counter } = this.state;
     const multiplicationTables = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
@@ -32,31 +44,53 @@ class App extends Component {
     };
 
     return (
-      <div className="App">
-        <h1> Multiplication App </h1>
-        <input
-          name="counter"
-          onChange={this.counterChange}
-          value={counter}
-          type="number"
-          style={{ width: "14%", height: "30px", fontSize: "25px" }}
-        />
-        <br /> <br />
+        <div className="multiplication-app">
+          <Grid container justify="center" className="">
+            <Grid item md={3} sm={6} xs={12}>
+               <Paper className="s" elevation={3}>
+                 <AppBar position="static" style={{backgroundColor:"#E3106D"}}>
+                  <Toolbar variant="dense">
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                      <MenuIcon />
+                    </IconButton>
+                    <Typography variant="h4" color="inherit" style={{margin: "0 auto"}}>
+                        Multiplication App 
+                    </Typography>
+                  </Toolbar>
+                </AppBar> <br/> 
+                  
+                  <TextField
+                    id="outlined-number"
+                    label="Number"
+                    type="number"
+                    variant="outlined"
+                    onChange={this.counterChange}
+                    value={counter}
+                  />
+                   <br /> <br />
 
-        <table style={tableStyle}>
-          <tbody>
-            <tr>
-              <th>Multiplication Table</th>
-            </tr>
-            {multiplicationTables.map((multiplicationTable, index) => (
-              <tr key={index}>
-                <td style={tableStyle}>
-                  {multiplicationTable} x {counter ? counter : 0} = {multiplicationTable * counter}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
+                  <table style={tableStyle}>
+                    <tbody>
+                      <tr>
+                        <th>Multiplication Table</th>
+                      </tr>
+                      {multiplicationTables.map((multiplicationTable, index) => (
+                        <tr key={index}>
+                          <td style={tableStyle}>
+                            {multiplicationTable} x {counter ? counter : 0} = {multiplicationTable * counter}
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table> <br /> <br />
+                  <BottomNavigation style={{backgroundColor:"#E3106D"}}>
+                    <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+                    <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+                    <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+                  </BottomNavigation>
+              </Paper>
+            </Grid>
+          </Grid>
       </div>
     );
   }
